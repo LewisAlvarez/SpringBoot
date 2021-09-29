8290296e6fd85029909c2e3134d3cf684365284f
LewisAlvarez <lewisalvarez99@gmail.com>
2021-09-29 10:20:06 -0500

    Inyeccion de dependencias ejemplo

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -6,7 +6,9 @@ Esta clase sirve para controlar las URL que se redirecciona el usuario en la pag
  */
 
 
+import com.cursojava.curso.dao.UsuarioDao;
 import com.cursojava.curso.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,12 @@ import java.util.List;
 @RestController
 
 public class UsuarioController {
+
+    /*
+    @Autowired --> Automaticamente hace que la clase UsuarioDaoImpl se cree un objeto y lo guarde dentro de la varibale usuarioDao.
+     */
+    @Autowired
+    private UsuarioDao usuarioDao;
 
     /*
     Para idicar la URL que deberia ser para que devuelva el contenido del método se usa @RequestMapping.
@@ -36,35 +44,7 @@ public class UsuarioController {
 
     @RequestMapping(value = "usuarios")
     public List<Usuario> getUsers(){
-
-        Usuario user1 = new Usuario();
-        user1.setId(123L);
-        user1.setName("Car");
-        user1.setLastName("Alvarez");
-        user1.setEmail("lewis040@hotmail.es");
-        user1.setPhoneNumber("31587699453");
-
-        Usuario user2 = new Usuario();
-        user2.setId(456L);
-        user2.setName("Juan");
-        user2.setLastName("Gutierrez");
-        user2.setEmail("juan123@hotmail.es");
-        user2.setPhoneNumber("38565153");
-
-        Usuario user3 = new Usuario();
-        user3.setId(789L);
-        user3.setName("Jose");
-        user3.setLastName("Piñeda");
-        user3.setEmail("jose@hotmail.es");
-        user3.setPhoneNumber("4156456453");
-
-        List<Usuario> users = new ArrayList<Usuario>();
-        users.add(user1);
-        users.add(user2);
-        users.add(user3);
-
-
-        return users;
+        return usuarioDao.getUsuarios();
     }
 
     @RequestMapping(value = "usuariohgj")
