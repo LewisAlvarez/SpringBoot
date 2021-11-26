6df759e2fede89170c87a64e2a5d46338b79ac5a
calvarezBOCC <calvarez@bancodeoccidente.com.co>
2021-11-26 09:47:32 -0500

    Initial commit

---
+++ src/test/java/com/example/apirestexample/ApirestexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apirestexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
