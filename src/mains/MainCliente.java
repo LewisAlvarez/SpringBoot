976781f50ab23f747139a4344ff26a55aa345cb3
lewisAlvarez <lewisalvarez99@gmail.com>
2021-12-02 17:17:47 -0500

    Consulta SELECT

--- src/mains/MainCliente.java
+++ src/mains/MainCliente.java
@@ -1,5 +1,7 @@
 package mains;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.cfg.Configuration;
 
@@ -11,9 +13,17 @@ public class MainCliente {
 
 	public static void main(String[] args) {
 		
-		Cliente ClienteUno = new Cliente("Lucas","Torreira","Call 123");
+		Cliente ClienteUno = new Cliente("Luz","Bejarano","Calle 8");
 		
-		guardarCliente(ClienteUno);
+		//guardarCliente(ClienteUno);
+		//consultarClientes();
+		
+		
+		//Consultar por direccio
+		String dir = "'Calle 7'";
+		consultarClienesEnDireccion(dir);
+		
+		
 	}
 	
 	public static org.hibernate.SessionFactory conexion() {
@@ -54,6 +64,62 @@ public class MainCliente {
 	}
 
 	
+	/**
+	 * Consulta de tipo SELECT sencillo
+	 */
+	
+	public static void consultarClientes() {
+		
+		org.hibernate.SessionFactory sf = conexion(); 
+		Session session = (((org.hibernate.SessionFactory) sf).openSession());
+		
+		try {
+			session.beginTransaction();
+			
+			//Consulta de todos los clientes
+			List<Cliente> clientes = session.createQuery("FROM Cliente").getResultList();
+			
+			recorrerClientes(clientes);
+			
+			
+			session.close();
+		}finally {
+			session.close();
+		}
+		
+	}
+	
+	
+	public static void consultarClienesEnDireccion(String direccion) {
+		org.hibernate.SessionFactory sf = conexion(); 
+		Session session = (((org.hibernate.SessionFactory) sf).openSession());
+		
+		try {
+			session.beginTransaction();
+			
+			//Consulta de todos los clientes que se encuentren en determinada direccion (parametro)
+			List<Cliente> clientes = 
+					session.createQuery("FROM Cliente cl WHERE cl.direccion="+direccion).getResultList();
+			
+			//Mostrar los clientes
+			recorrerClientes(clientes);
+			
+			System.out.println("Total de clientes en direccion: " + clientes.size()); 
+			
+			//commit
+			session.getTransaction().commit();
+			
+			session.close();
+		}finally {
+			session.close();
+		}
+	}
+
+	private static void recorrerClientes(List<Cliente> clientes) {
+		for(Cliente c : clientes) {
+			System.out.println(c.toString());
+		}
+	}
 	
 	
 }
